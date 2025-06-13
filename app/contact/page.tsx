ba841c4ccbcafd2650cf272ae9c94797a642a713
Md Shadik <md.shadik@unthinkable.co>
2025-06-13 15:33:14 +0530

    Change title and fav ico

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,9 @@
 import ContactForm from "@/views/contact";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Contact - Md Shadik",
+  };
 
 export default function ContactPage() {
   return <ContactForm />;
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,4 +1,9 @@
 import Projects from "@/views/projects";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Projects - Md Shadik",  
+}
 
 const ProjectsPage = () => {
   return <Projects />;
