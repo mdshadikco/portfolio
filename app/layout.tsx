e52efaa7a02bea9c25e8188d54d02ec5d0354234
Md Shadik <md.shadik@unthinkable.co>
2025-06-13 17:43:42 +0530

    Add Header with mobile responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/component/header";
 import Link from "next/link";
 import { FolderOpen, Home, Mail } from "lucide-react";
 
@@ -29,31 +30,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <header
+        <Header />
+        {/* <header
           className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 'bg-slate-900/95 backdrop-blur-md border-b border-white/10 shadow-lg shadow-purple-500/10`}
         >
           <div className="flex justify-center items-center p-2 gap-4">
-            <Link
-              href={"/"}
-              className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"
-            >
-              <Home className="w-4 h-4" /> Home
-            </Link>
-            <Link
-              href={"/projects"}
-              className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"
-            >
-              <FolderOpen className="w-4 h-4" /> Projects
-            </Link>
-            <Link
-              href={"/contact"}
-              className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"
-            >
-              <Mail className="w-4 h-4" />
-              Contacts
-            </Link>
+          <Link href={"/"} className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"><Home className="w-4 h-4"/> Home</Link>
+          <Link href={"/projects"} className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"><FolderOpen className="w-4 h-4"/> Projects</Link>
+          <Link href={"/contact"} className="flex gap-2 border border-transparent items-center hover:border-b-slate-300 transition-all duration-300"><Mail className="w-4 h-4" />Contacts</Link>
           </div>
-        </header>
+        </header> */}
         {children}
       </body>
     </html>
